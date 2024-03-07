f560c0819be9de847477ce93cfa97d9d7b95f3b5
Dogancem Duran <dogancemduran@gmail.com>
2024-03-07 12:36:25 +0100

    added external CSS

--- hw2/contacts.php
+++ hw2/contacts.php
@@ -1,44 +1,47 @@
 <html>
     <head>
         <title>Contacts</title>
+        <link rel="stylesheet" href="phoneBook.css">
     </head>
     <body>
-        <h1>Contacts</h1>
-        <button onclick="window.location.href='index.html'">Home</button><br>
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Phone</th>
-                <th>Address</th>
-            </tr>
-            <?php
-                function printContacts($contacts){
-                    foreach($contacts as $contact){
-                        echo "<tr>";
-                        echo "<td>".$contact['name']."</td>";
-                        echo "<td>".$contact['phone']."</td>";
-                        echo "<td>".$contact['address']."</td>";
-                        echo "</tr>";
-                    }
-                }
-                function readContacts(){
-                    $file = fopen("contacts.txt", "r");
-                    $contacts = [];
-                    while(!feof($file)){
-                        $name = mb_convert_encoding(trim(fgets($file)),"UTF-8");
-                        $phone = mb_convert_encoding(trim(fgets($file)),"UTF-8");
-                        $address = mb_convert_encoding(trim(fgets($file)),"UTF-8");
-                        if($name != ""){
-                            $contact = ['name' => $name, 'phone' => $phone, 'address' => $address];
-                            array_push($contacts, $contact);
+        <div>
+            <h1 class=horizontalCenter>Contacts</h1>
+            <div class=horizontalCenter><button onclick="window.location.href='index.html'">Home</button></div><br>
+            <table class=horizontalCenter>
+                <tr>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                </tr>
+                <?php
+                    function printContacts($contacts){
+                        foreach($contacts as $contact){
+                            echo "<tr>";
+                            echo "<td>".$contact['name']."</td>";
+                            echo "<td>".$contact['phone']."</td>";
+                            echo "<td>".$contact['address']."</td>";
+                            echo "</tr>";
                         }
                     }
-                    fclose($file);
-                    return $contacts;
-                }
-                $contacts = readContacts();
-                printContacts($contacts);
-            ?>
-        </table>
+                    function readContacts(){
+                        $file = fopen("contacts.txt", "r");
+                        $contacts = [];
+                        while(!feof($file)){
+                            $name = mb_convert_encoding(trim(fgets($file)),"UTF-8");
+                            $phone = mb_convert_encoding(trim(fgets($file)),"UTF-8");
+                            $address = mb_convert_encoding(trim(fgets($file)),"UTF-8");
+                            if($name != ""){
+                                $contact = ['name' => $name, 'phone' => $phone, 'address' => $address];
+                                array_push($contacts, $contact);
+                            }
+                        }
+                        fclose($file);
+                        return $contacts;
+                    }
+                    $contacts = readContacts();
+                    printContacts($contacts);
+                ?>
+            </table>
+        </div>    
     </body>
 </html>
--- hw2/search.php
+++ hw2/search.php
@@ -1,43 +1,50 @@
 <html>
     <head>
         <title>Search</title>
+        <link rel="stylesheet" href="phoneBook.css">
     </head>
     <body>
-        <h1>Search</h1>
-        <button onclick="window.location.href='index.html'">Home</button><br>
-        <?php
-            if($_POST['search'] == ""){
-                echo "<h2>Enter a name to search</h2>";
-                return;
-            }
-            $file = fopen("contacts.txt", "r",);
-            $is_found = false;
-            $search = mb_convert_encoding($_POST['search'],'UTF-8');
-            while(!feof($file)){
-                $name = mb_convert_encoding(trim(fgets($file)),"UTF-8");
-                $phone = mb_convert_encoding(trim(fgets($file)),"UTF-8");
-                $address = mb_convert_encoding(trim(fgets($file)),"UTF-8");
-                if(strcmp($name,$search)==0){
-                    echo "<table>";
-                    echo "<tr>";
-                    echo "<th>Name</th>";
-                    echo "<th>Phone</th>";
-                    echo "<th>Address</th>";
-                    echo "</tr>";
-                    echo "<tr>";
-                    echo "<td>".$name."</td>";
-                    echo "<td>".$phone."</td>";
-                    echo "<td>".$address."</td>";
-                    echo "</tr>";
-                    echo "</table>";
-                    $is_found = true;
-                    break;
+        <div>
+            <h1 class=horizontalCenter>Search</h1>
+            <div class = horizontalCenter>
+                <button onclick="window.location.href='index.html'">Home</button><br>
+            </div>
+            <div class=horizontalCenter>
+            <?php
+                if($_POST['search'] == ""){
+                    echo "<h2>Enter a name to search</h2>";
+                    return;
                 }
-            }
-            fclose($file);
-            if(!$is_found){
-                echo "<h2>Contact not found</h2>";
-            }
-        ?>
+                $file = fopen("contacts.txt", "r",);
+                $is_found = false;
+                $search = mb_convert_encoding($_POST['search'],'UTF-8');
+                while(!feof($file)){
+                    $name = mb_convert_encoding(trim(fgets($file)),"UTF-8");
+                    $phone = mb_convert_encoding(trim(fgets($file)),"UTF-8");
+                    $address = mb_convert_encoding(trim(fgets($file)),"UTF-8");
+                    if(strcmp($name,$search)==0){
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>Name</th>";
+                        echo "<th>Phone</th>";
+                        echo "<th>Address</th>";
+                        echo "</tr>";
+                        echo "<tr>";
+                        echo "<td>".$name."</td>";
+                        echo "<td>".$phone."</td>";
+                        echo "<td>".$address."</td>";
+                        echo "</tr>";
+                        echo "</table>";
+                        $is_found = true;
+                        break;
+                    }
+                }
+                fclose($file);
+                if(!$is_found){
+                    echo "<h2>Contact not found</h2>";
+                }
+            ?>
+            </div>
+        </div>
     </body>
-
+</html>
